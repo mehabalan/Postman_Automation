bc2ac807461f3523d1b67a777d0d6ec46fc8a70e
mehabalan <102800071+mehabalan@users.noreply.github.com>
2026-03-26 09:57:13 +0530

    Delete Assignment 2.py

--- Assignment 2.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Enter the first number (a): "))
-b = float(input("Enter the second number (b): "))
-floor_division_result = a // b
-power_result = a ** b
-are_equal = a == b
-print("1. Floor Division (a // b):", floor_division_result)
-print("2. a power b (a ** b):", power_result)
-print("3. Are a and b equal:", are_equal)
